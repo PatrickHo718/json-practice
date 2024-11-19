f9fce128ca8fb09bc3e33ed0ec757c596473d43d
root <root@Patrick>
2024-11-19 16:25:17 -0500

    edit script2

--- labfolder/lab8/script2.py
+++ labfolder/lab8/script2.py
@@ -9,12 +9,10 @@ with open('../../data/schacon.repos.json', 'r') as file:
 with open("chacon.csv", "w", newline="") as output:
     csv_writer = csv.writer(output)
 
-    csv_writer.writerow(["name","html_url","updated_at","visibility"])
-
     for i, repo in enumerate(data[:5]):
        csv_writer.writerow([
           repo.get("name", ""),
           repo.get("html_url", ""),
-          repo.get("updated_url",""),
-          repo.get("visibilty_url","")
+          repo.get("updated_at",""),
+          repo.get("visibility","")
     ])
